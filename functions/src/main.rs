26bd7d09ea71d2c876f9ff89d91e9e70d39d0c52
thinhvqnguyen <thinhvqnguyen@kobiton.com>
2021-01-09 22:33:30 +0700

    functions and function arguments

---
+++ functions/src/main.rs
@@ -0,0 +1,32 @@
+fn println_value(x: i32)
+{
+    println!("value = {}", x);
+}
+
+fn increase(x: &mut i32)
+{
+    *x += 1;
+}
+
+fn product(a: i32, b: i32) -> i32
+{
+    a * b
+}
+
+fn functions()
+{
+    println_value(22);
+
+    let mut x = 34;
+    increase(&mut x);
+    println!("x = {}", x);
+
+    let a = 3;
+    let b = 5;
+    let value = product(a, b);
+    println!("product = {}", value);
+}
+
+fn main() {
+    functions();
+}

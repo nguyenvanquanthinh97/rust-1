f2fe7b77822a70d8db6206eabbd3a4841a8b1bb7
thinhvqnguyen <thinhvqnguyen@kobiton.com>
2021-02-11 16:05:38 +0700

    atomic reference counted variables

--- lifetime_and_memory/src/main.rs
+++ lifetime_and_memory/src/main.rs
@@ -3,11 +3,13 @@ mod borrowing;
 mod lifetime;
 mod lifetime_in_structure_implementation;
 mod reference_counted_variables;
+mod atomic_reference_counted_variables;
 
 fn main() {
     // ownership::main();
     // borrowing::main();
     // lifetime::main();
     // lifetime_in_structure_implementation::main();
-    reference_counted_variables::main();
+    // reference_counted_variables::main();
+    atomic_reference_counted_variables::main();
 }

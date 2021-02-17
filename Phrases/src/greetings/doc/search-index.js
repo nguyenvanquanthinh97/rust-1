5511ceaa64232e857cbc19e9647cc96da530dd72
thinhvqnguyen <thinhvqnguyen@kobiton.com>
2021-02-17 13:57:56 +0700

    rust_docs

---
+++ Phrases/src/greetings/doc/search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"english":{"doc":"This module contains English phrases.","i":[[5,"hello","english","Applies to code below In this case it \'s our function…",null,[[],["string",3]]],[5,"goodbye","","",null,[[],["string",3]]]],"p":[]}\
+}');
+addSearchOptions(searchIndex);initSearch(searchIndex);
--- Phrases/src/greetings/english.rs
+++ Phrases/src/greetings/english.rs
@@ -1,2 +1,16 @@
+//! This module contains English phrases.
+//! 
+//! # Examples
+//! ```
+//! let username = "John";
+//! println!("{}, {}!",
+//!   Phrases::greetings::english::hello(),
+//!   username
+//! );
+//! ```
+
+
+/// Applies to code below
+/// In this case it 's our function `hello()` function.
 pub fn hello() -> String {"hello".to_string()}
 pub fn goodbye() -> String {"bye".to_string()}
